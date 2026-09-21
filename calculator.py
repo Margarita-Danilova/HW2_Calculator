324a42e606ea96a0fab8f482a74693916cc88c1e
nastyaprok <prokanvad@gmail.com>
2026-09-21 23:09:38 +0300

    Add calculator script, modify documentation, and delete old image files

---
+++ calculator.py
@@ -0,0 +1,23 @@
+def main():
+    input_string = input().split()
+
+    # Преобразуем ввод float или int
+    in_a, op, in_b = input_string
+    a = float(in_a) if "." in in_a else int(in_a)
+    b = float(in_b) if "." in in_b else int(in_b)
+
+    #Заготовка для функций, вставьте свое название.
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide,
+    }
+
+    if op in operations:
+        result = operations[op](a, b)
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
